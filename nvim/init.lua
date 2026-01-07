7431997944c5081483dacdbc7e1e45d953e59429
lesguillemets <kgkacko+github@gmail.com>
2026-01-07 20:54:03 +0900

    highlight yanked area

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
 vim.cmd.source("~/.vim/base_config.vim")
+
 require("config.lazy")
 vim.cmd("colorscheme nightfox")
+
+-- https://eiji.page/blog/neovim-highlight-on-yank/ and :h on_yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.hl.on_yank({ higroup='Visual', timeout = 300 })
+  end,
+})
