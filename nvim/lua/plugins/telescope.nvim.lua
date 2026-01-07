7482ae0d7c5828e680b2f50cc60b5b8519bf00ad
lesguillemets <kgkacko+github@gmail.com>
2026-01-07 15:14:43 +0900

    telescope: invoke with ctrl+P

--- nvim/lua/plugins/telescope.nvim.lua
+++ nvim/lua/plugins/telescope.nvim.lua
@@ -4,5 +4,9 @@ return {
 		'nvim-lua/plenary.nvim',
 		-- optional but recommended
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	}
+	},
+	config = function()
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'telescope find files'})
+	end
 }
