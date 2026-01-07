b726ab510926c185c9f0a5f76f910e8228345aac
lesguillemets <kgkacko+github@gmail.com>
2026-01-07 13:08:10 +0900

    prefer opts over config = function

--- nvim/lua/plugins/nightfox.nvim.lua
+++ nvim/lua/plugins/nightfox.nvim.lua
@@ -1,10 +1,8 @@
 return {
 	"EdenEast/nightfox.nvim",
-	config = function()
-		require('nightfox').setup({
-			options = {
-				transparent = true,
-			}
-		})
-	end
+	opts = {
+		options = {
+			transparent = true,
+		}
+	}
 }
