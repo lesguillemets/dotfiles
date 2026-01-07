b273361ed15bca0d6fc58e053fbe3d7e28747255
lesguillemets <kgkacko+github@gmail.com>
2026-01-07 21:19:16 +0900

    somehow this caused an error

--- nvim/lua/plugins/telescope.nvim.lua
+++ nvim/lua/plugins/telescope.nvim.lua
@@ -1,5 +1,6 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '*',
+	'nvim-telescope/telescope.nvim',
+	-- tag = '*',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		-- optional but recommended
