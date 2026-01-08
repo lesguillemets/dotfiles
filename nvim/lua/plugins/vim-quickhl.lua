76772fcf3062314ee39200ebe5e1d15bc3ad589d
lesguillemets <kgkacko+github@gmail.com>
2026-01-08 22:58:28 +0900

    quickhl

---
+++ nvim/lua/plugins/vim-quickhl.lua
@@ -0,0 +1,7 @@
+return {
+	"t9md/vim-quickhl",
+	keys = {
+		{ "<space>m", "<Plug>(quickhl-manual-this)", mode= {"n", "x"} },
+		{ "<space>M", "<Plug>(quickhl-manual-reset)", mode= {"n", "x"} },
+	}
+}
