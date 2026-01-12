4164a652b1d8ce87a5e654cb8a7692b60fb5ee74
lesguillemets <kgkacko+github@gmail.com>
2026-01-12 15:10:43 +0900

    Fern

---
+++ nvim/lua/plugins/fern-vim.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'lambdalisue/vim-fern',
+  },
+  {
+    'lambdalisue/vim-fern-git-status',
+    dependencies = { "lambdalisue/vim-fern" },
+  },
+  {
+    'lambdalisue/vim-fern-hijack',
+    dependencies = { "lambdalisue/vim-fern" },
+  }
+}
