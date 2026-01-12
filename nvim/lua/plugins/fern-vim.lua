21e5c19d2d020793c9cdcb9ed037fa9a0c72e937
lesguillemets <kgkacko+github@gmail.com>
2026-01-12 16:40:52 +0900

    configurations

--- nvim/lua/plugins/fern-vim.lua
+++ nvim/lua/plugins/fern-vim.lua
@@ -1,6 +1,17 @@
 return {
   {
     'lambdalisue/vim-fern',
+    config = function()
+      -- l で open までは行ってほしくないので， expand までのマップに上書きする
+      vim.api.nvim_create_augroup('vim-fern-config', {})
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'fern' },
+        group = 'vim-fern-config',
+        command = "nmap <buffer><nowait> l <Plug>(fern-action-expand)"
+      })
+      -- show dot files
+      vim.g['fern#default_hidden'] = 1
+    end
   },
   {
     'lambdalisue/vim-fern-git-status',
