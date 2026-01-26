9c2cab789d852dff60999889e9717e3778efd458
lesguillemets <kgkacko+github@gmail.com>
2026-01-26 16:20:21 +0900

    use typescript language server

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.lsp.enable({'rust_analyzer', 'pyright', 'hls', 'lua_ls', 'marksman'})
+    vim.lsp.enable( {
+      'rust_analyzer',
+      'pyright',
+      'hls',
+      'lua_ls',
+      'marksman',
+      'ts_ls'
+    })
   end
 }
 
