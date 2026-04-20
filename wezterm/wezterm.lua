0ecab0af9e74d91dc9f22397362b1c2e087fef8f
lesguilemets <kgkacko+github@gmail.com>
2026-04-20 14:46:22 +0900

    wezterm: disable audible bell & larger fontsize

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.audible_bell = "Disabled"
 
 config.initial_cols = 120
 config.initial_rows = 28
@@ -12,7 +13,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_ime = true
 config.default_domain = 'WSL:Ubuntu-24.04'
 
-config.font_size = 10
+config.font_size = 13
 config.font = wezterm.font 'Cascadia Mono PL'
 config.color_scheme = 'One Half Black (Gogh)'
 config.window_background_opacity = 0.9
